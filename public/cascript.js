905a4f23729f44386716490c4af04872cdfb3ac4
Rishabh sharma <rish6696.rs@gmail.com>
2019-04-06 19:01:54 +0530

    corrected

--- public/cascript.js
+++ public/cascript.js
@@ -35,7 +35,7 @@ $(()=>{
    
 })
 $(document).on('click','.downbuttons',function(){
-    window.location=`https://yourdocman.herokuapp.com//requests/getfile?filename=${$(this).val()}`
+    window.location=`https://yourdocman.herokuapp.com/requests/getfile?filename=${$(this).val()}`
     //console.log($(this).val())
 });
 
